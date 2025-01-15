f8c0c025d35a18b3fcbd7f5416e44493a169d574
aliraj313 <alirajabi0151@gmail.com>
2025-01-15 14:08:33 +0330

    chore : add proxy in producer and consumer

--- consumer_service/src/app.service.ts
+++ consumer_service/src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  handleMessage(data: any) {
+    console.log('Received message:', data);
   }
+ 
 }
