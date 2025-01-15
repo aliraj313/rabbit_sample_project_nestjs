baeacad9969ac3fc6720b671f033727445e6846f
aliraj313 <alirajabi0151@gmail.com>
2025-01-15 14:10:40 +0330

    chore :add proxy in producer and consumer

--- producer_service/src/app.module.ts
+++ producer_service/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'RABBITMQ_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672'], // Replace 'rabbitmq' with 'localhost' if running locally
+          queue: 'messages_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- producer_service/src/app.service.ts
+++ producer_service/src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('RABBITMQ_SERVICE') private readonly client: ClientProxy,
+  ) {}
+  sendMessage() {
+    this.client.emit('send_message_event', {
+      text: 'Hello from Producer!',
+    });
+    return { message: 'message sended successfully' };
   }
 }
